596e332f42762ee1357a0e46fdf92457c07118fd
LetsWalkInLine <1327604853@qq.com>
2026-04-15 16:13:06 +0800

    完成输出颜色

--- arceos/exercises/print_with_color/src/main.rs
+++ arceos/exercises/print_with_color/src/main.rs
@@ -6,5 +6,9 @@ use axstd::println;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    println!("[WithColor]: Hello, Arceos!");
+    println!("\x1b[31mred!\x1b[0m");
+    println!("\x1b[32mgreen!\x1b[0m");
+    println!("\x1b[34mblue!\x1b[0m");
+
+    println!("\x1b[31m[red]: Hello, Arceos!\x1b[0m");
 }
