080bf42d3e6b5efb6fa6f83b371674de327dc3a5
LetsWalkInLine <1327604853@qq.com>
2026-04-21 13:59:55 +0800

    feat: m_1_0实现处理page fault

--- arceos/tour/m_1_0/src/syscall.rs
+++ arceos/tour/m_1_0/src/syscall.rs
@@ -1,8 +1,11 @@
 #![allow(dead_code)]
 
-use axhal::arch::TrapFrame;
-use axhal::trap::{register_trap_handler, SYSCALL};
 use axerrno::LinuxError;
+use axhal::arch::TrapFrame;
+use axhal::mem::VirtAddr;
+use axhal::paging::MappingFlags;
+use axhal::trap::{register_trap_handler, PAGE_FAULT, SYSCALL};
+use axtask::{self, TaskExtRef};
 
 const SYS_EXIT: usize = 93;
 
@@ -13,11 +16,39 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
         SYS_EXIT => {
             ax_println!("[SYS_EXIT]: process is exiting ..");
             axtask::exit(tf.arg0() as _)
-        },
+        }
         _ => {
             ax_println!("Unimplemented syscall: {}", syscall_num);
             -LinuxError::ENOSYS.code() as _
         }
     };
     ret
+}
+
+#[register_trap_handler(PAGE_FAULT)]
+fn handle_page_fault(va: VirtAddr, flags: MappingFlags, is_user: bool) -> bool {
+    ax_println!("handle_page_fault ...");
+    ax_println!("Hi there, Let me help you");
+
+    if !is_user {
+        return false;
+    }
+
+    let is_successfully = axtask::current()
+        .task_ext()
+        .aspace
+        .lock()
+        .handle_page_fault(va, flags);
+
+    if !is_successfully {
+        ax_println!(
+            "Page fault cannot be handled! va={:#x?}, flags={:?}",
+            va,
+            flags
+        );
+    } else {
+        ax_println!("{}: handle page fault OK!", axtask::current().id_name());
+    }
+
+    is_successfully
 }
--- arceos/update_disk.sh
+++ arceos/update_disk.sh
@@ -1,27 +1,45 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-if [ $# -ne 1 ]; then
-    printf "Usage: ./update.sh [userapp path]\n"
-    exit
+set -eu
+
+if [ "$#" -ne 1 ]; then
+    printf "Usage: ./update_disk.sh [userapp path]\n"
+    exit 1
 fi
 
-FILE=$1
+FILE="$1"
 
-if [ ! -f $FILE ]; then
-    printf "File '$FILE' doesn't exist!\n"
-    exit
+if [ ! -f "$FILE" ]; then
+    printf "File '%s' doesn't exist!\n" "$FILE"
+    exit 1
 fi
 
 if [ ! -f ./disk.img ]; then
     printf "disk.img doesn't exist! Please 'make disk_img'\n"
-    exit
+    exit 1
 fi
 
-printf "Write file '$FILE' into disk.img\n"
+if [ "$(id -u)" -eq 0 ]; then
+    SUDO=""
+elif command -v sudo >/dev/null 2>&1; then
+    SUDO="sudo"
+else
+    printf "Need root privilege (run as root or install sudo).\n"
+    exit 1
+fi
+
+cleanup() {
+    if [ -d ./mnt ]; then
+        $SUDO umount ./mnt >/dev/null 2>&1 || true
+        rm -rf ./mnt
+    fi
+}
+
+trap cleanup EXIT
+
+printf "Write file '%s' into disk.img\n" "$FILE"
 
 mkdir -p ./mnt
-sudo mount ./disk.img ./mnt
-sudo mkdir -p ./mnt/sbin
-sudo cp $FILE ./mnt/sbin
-sudo umount ./mnt
-rm -rf mnt
+$SUDO mount ./disk.img ./mnt
+$SUDO mkdir -p ./mnt/sbin
+$SUDO cp "$FILE" ./mnt/sbin
